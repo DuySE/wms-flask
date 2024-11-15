b4bcc8ce5724c44bc1d3fd4908286f56b4164ed7
DuySE <duynse@gmail.com>
2024-11-14 19:50:50 -0800

    fix: edit import

--- user/user_model.py
+++ user/user_model.py
@@ -1,12 +1,11 @@
 import uuid
 
-from flask_mongoengine import Document
-from mongoengine import StringField
+from mongoengine import Document, StringField
 
 class User(Document):
     user_id = StringField(primary_key=True, default=lambda: str(uuid.uuid4()))
-    email = StringField(require=True, unique=True)
-    user_name = StringField(require=True, unique=True)
+    email = StringField(required=True, unique=True)
+    user_name = StringField(required=True, unique=True)
     password = StringField(required=True)
     role = StringField(default='user')
 
